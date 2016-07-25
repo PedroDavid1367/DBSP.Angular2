d8f7127293fdda76be1de568f0fb8fd868246872
David1367 <pedro.571.david@gmail.com>
2016-07-25 16:33:34 -0400

    fix

---
+++ DBSP.Angular2.TourOfHeores/app/Oauth2/oauth2.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+  selector: "oauth2",
+  template: `
+  <h3>Oauth2 come on!</h3>
+  `
+})
+export class Oauth2COmponent {
+  
+}
--- DBSP.Angular2.TourOfHeores/app/app.component.ts
+++ DBSP.Angular2.TourOfHeores/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from "@angular/core";
-import { HeroListComponent } from "./Heroes/hero-list.component";
-import { IHeroService, HeroService } from "./Heroes/hero.service";
+import { Component }                  from "@angular/core";
+import { HeroListComponent }          from "./Heroes/hero-list.component";
+import { IHeroService, HeroService }  from "./Heroes/hero.service";
+
+import { Oauth2COmponent }            from "./Oauth2/oauth2.component";                
 // External sample
-import { TreeViewDemo } from "./Directories/tree-view-demo.component";
-import { TestingDirectives } from "./Directories/testing-directives.component";
+import { TreeViewDemo }               from "./Directories/tree-view-demo.component";
+import { TestingDirectives }          from "./Directories/testing-directives.component";
 
 @Component({
   selector: "app",
+  //template: `
+  //<div class="container">
+  //  <h1>A nooby Hero list</h1>
+  //  <hero-list></hero-list>
+  //</div>
+  //<testing-directives></testing-directives>
+  //<!--<tree-view-demo></tree-view-demo>-->
+  //`,
   template: `
-  <div class="container">
-    <h1>A nooby Hero list</h1>
-    <hero-list></hero-list>
-  </div>
-  <testing-directives></testing-directives>
-  <!--<tree-view-demo></tree-view-demo>-->
+  <oauth2></oauth2>
   `,
-  directives: [HeroListComponent, TreeViewDemo, TestingDirectives],
+  directives: [HeroListComponent, TreeViewDemo, TestingDirectives, Oauth2COmponent],
   //providers: [{ provide: IHeroService, useClass: HeroService }]
   providers: [{ provide: IHeroService, useClass: HeroService }]
 })
